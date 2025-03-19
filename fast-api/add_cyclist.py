1a1354bfac0037ab8d0998e83a5b1251fafa66f0
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2025-03-19 10:17:00 +0100

    creation et alimentation des bdd

--- fast-api/add_cyclist.py
+++ fast-api/add_cyclist.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 
-conn = sqlite3.connect('../database.db')
+conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
 cyclists_data = [
--- fast-api/add_user.py
+++ fast-api/add_user.py
@@ -1,9 +1,9 @@
 import sqlite3
-
+import hashlib
 # Connexion à la base de données
 
 
-conn = sqlite3.connect('../database.db')
+conn = sqlite3.connect('database.db')
 cur = conn.cursor()
 
 # Sélectionner tous les cyclistes de la table "cyclist"
@@ -14,8 +14,10 @@ cyclists = cur.fetchall()
 for cyclist in cyclists:
     cyclist_id = cyclist[0]
     username = f"user_{cyclist_id}"  # Générer le nom d'utilisateur sous la forme user_<id>
-    password = "azerty"  # Le mot de passe est toujours 'azerty'
+    mdp = 'azerty'
+    password = hashlib.sha256(mdp.encode('utf-8')).hexdigest()  # Le mot de passe est toujours 'azerty'
     
+
     # Insérer l'utilisateur dans la table "user"
     cur.execute("""
     INSERT INTO user (cyclist_id, username, password)
